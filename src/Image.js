ef270a07d9d2f6d28ae7f6e0cf767feb75b1c5d1
github username <github email address>
2018-04-13 10:37:41 -0400

    my solution

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,25 @@
 class Image {
-  
 
+  constructor(id, src, name, likes, comments){
+    this.id = id
+    this.src = src
+    this.name = name
+    this.likes = likes
+    this.comments = comments
+  }
+
+  render(){
+    let image = document.getElementById('image')
+    let name = document.getElementById('name')
+    let likes = document.getElementById('likes')
+    let comments = document.getElementById('comments')
+    comments.innerHTML = ""
+    image.src = this.src
+    name.innerText = this.name
+    likes.innerHTML = this.likes
+    this.comments.forEach((comment)=>{
+      comments.innerHTML += `<li id = "${comment.id}-comment"> ${comment.content}  <button id="delete-${comment.id}">destroy</button></li>`
+    })
+
+  }
 }
