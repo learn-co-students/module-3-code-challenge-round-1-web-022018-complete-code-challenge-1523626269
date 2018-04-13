a072d5ff85af0f2e15ee517c86c352139eca983e
lidianvilazio <lidianvilazio@hotmail.com>
2018-04-13 11:04:33 -0400

    Done.

--- src/Image.js
+++ src/Image.js
@@ -1,4 +1,14 @@
+
+let s = {images: []}
+
 class Image {
-  
+
+  constructor(id,url, name, like_count){
+    this.id = id
+    this.url = url
+    this.name = name
+    this.like_count = like_count
+    s.images.push(this)
+  }
 
 }
--- src/index.js
+++ src/index.js
@@ -4,4 +4,66 @@ document.addEventListener('DOMContentLoaded', function() {
   const likeURL = `https://randopic.herokuapp.com/likes/`
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  let name = document.getElementById('name')
+  let image = document.getElementById('image')
+  let likes = document.getElementById('like_button')
+  let count = document.getElementById('likes')
+  let comment = document.getElementById('comment_form')
+  let ul = document.getElementById('comments')
+
+  fetch(imageURL).then(r => r.json()).then(json => {
+    // let ob = new Image(json.id, json.url, json.name, json.like_count)
+    image.src = json.url
+    name.innerHTML = json.name
+    likes.addEventListener('click', e => {
+      let times = json.like_count++
+      count.innerHTML = json.like_count
+      createLike(json)
+    })
+
+    comment.addEventListener('submit', e => {
+      let list = document.createElement('li')
+      list.innerHTML = `<h4>${e.target.comment_input.value}</h4>`
+      ul.append(list)
+      createComment(e.target.comment_input.value)
+      e.preventDefault()
+    })
+  })
+
+  function createLike(json) {
+    let config = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({image_id: json.id})
+    }
+    fetch(likeURL,config)
+  }
+
+  function createComment(content) {
+    let config = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({content: content, image_id: imageId})
+    }
+    fetch(commentsURL,config)
+  }
+  // function update(json) {
+  //   let config = {
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({like_count: json.like_count, comments: json.comments})
+  //   }
+  //   fetch(imageURL,config).then(r => r.json()).then(json => console.log(json))
+  // }
+
+
 })
