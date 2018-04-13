95e696f678678ab566b1a8fb10e8109b6947f214
alec-petros <a.petros@verizon.net>
2018-04-13 10:31:12 -0400

    cleaned up some unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   const imageId = 9 //Enter your assigned imageId here
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-  let activeImage = null
 
 
   function loadImg() {
     fetch(imageURL).then(r => r.json()).then(json => {
       img = new Image(json)
-      activeImage = img
       img.render()
       img.initLikeListener()
-      Comment.initCommentListener(activeImage)
+      Comment.initCommentListener(img)
     })
   }
 
