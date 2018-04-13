2dab27b88f75c445b698962e966efd0ed621a4b4
alec-petros <a.petros@verizon.net>
2018-04-13 10:25:40 -0400

    Done

--- src/Comment.js
+++ src/Comment.js
@@ -1,3 +1,52 @@
+const commentsURL = `https://randopic.herokuapp.com/comments/`
+
 class Comment {
-  
+  constructor(obj) {
+    this.content = obj.content
+    this.id = obj.id
+    this.image_id = obj.image_id
+  }
+
+  static initCommentListener(img) {
+    $('#comment_form').on('submit', e => {
+      e.preventDefault();
+      let comment = new Comment({content: $('#comment_input').val(), id: null, image_id: img.id})
+      $('#comments').append(comment.render())
+      $('#comment_input').val('')
+      img.comments.push(comment)
+      comment.persistComment();
+    })
+  }
+
+  initDeleteListener() {
+    $(`#delete-${this.id}`).on('click', e => {
+      $(`#comment-${this.id}`).remove()
+      fetch(commentsURL+`/${this.id}`, {
+        method: "DELETE"
+      }).then(r => r.json()).then(json => console.log(json))
+    })
+  }
+
+  persistComment() {
+    fetch(commentsURL, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        image_id: this.image_id,
+        content: this.content
+      })
+    }).then(r => r.json()).then(json => console.log(json))
+  }
+
+  render() {
+    $('#comments').append(`<li id="comment-${this.id}">${this.content} <button id="delete-${this.id}">Delete</button> </li>`)
+    this.initDeleteListener()
+  }
+
+  setImage() {
+
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 9 //Enter your assigned imageId here
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
-  const likeURL = `https://randopic.herokuapp.com/likes/`
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+  let activeImage = null
+
+
+  function loadImg() {
+    fetch(imageURL).then(r => r.json()).then(json => {
+      img = new Image(json)
+      activeImage = img
+      img.render()
+      img.initLikeListener()
+      Comment.initCommentListener(activeImage)
+    })
+  }
+
+  loadImg()
 
 })
