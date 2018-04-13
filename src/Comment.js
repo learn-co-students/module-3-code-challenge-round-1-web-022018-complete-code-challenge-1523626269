f6ea8c740a169d14d3b608754bd7a8c55824b92d
alec-petros <a.petros@verizon.net>
2018-04-13 10:30:12 -0400

    Fixed weird bug with enter keypress

--- src/Comment.js
+++ src/Comment.js
@@ -8,6 +8,12 @@ class Comment {
   }
 
   static initCommentListener(img) {
+    $('.input').keypress(function (e) {
+      if (e.which == 13) {
+        $('#comment_form').submit();
+        return false;
+      }
+    });
     $('#comment_form').on('submit', e => {
       e.preventDefault();
       let comment = new Comment({content: $('#comment_input').val(), id: null, image_id: img.id})
@@ -23,7 +29,7 @@ class Comment {
       $(`#comment-${this.id}`).remove()
       fetch(commentsURL+`/${this.id}`, {
         method: "DELETE"
-      }).then(r => r.json()).then(json => console.log(json))
+      })
     })
   }
 
@@ -38,7 +44,7 @@ class Comment {
         image_id: this.image_id,
         content: this.content
       })
-    }).then(r => r.json()).then(json => console.log(json))
+    })
   }
 
   render() {
@@ -46,7 +52,4 @@ class Comment {
     this.initDeleteListener()
   }
 
-  setImage() {
-
-  }
 }
